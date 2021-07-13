5ef7115324a98a351a1b0dd6202c0750552fd5b4
DavidBai <baiminghan@gmail.com>
2021-07-13 12:33:53 +1200

    first trail of deploy

---
+++ src/components/library.js
@@ -0,0 +1,33 @@
+import React from "react";
+import LibrarySong from "./librarySong";
+
+const library = ({
+  songs,
+  setCurrentSong,
+  audioRef,
+  isPlaying,
+  setSongs,
+  libraryStatus,
+}) => {
+  return (
+    <div className={`library ${libraryStatus ? "active-library" : ""}`}>
+      <h2>Library</h2>
+      <div className="library-songs">
+        {songs.map((song) => (
+          <LibrarySong
+            songs={songs}
+            setSongs={setSongs}
+            song={song}
+            setCurrentSong={setCurrentSong}
+            id={song.id}
+            key={song.id}
+            audioRef={audioRef}
+            isPlaying={isPlaying}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default library;
---
+++ src/components/librarySong.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+const LibrarySong = ({
+  songs,
+  song,
+  setCurrentSong,
+  id,
+  audioRef,
+  isPlaying,
+  setSongs,
+}) => {
+  const songSelectHandler = async () => {
+    await setCurrentSong(song);
+
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+
+    // check if the song is playing
+    if (isPlaying) audioRef.current.play();
+  };
+
+  return (
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? "selected" : ""}`}
+    >
+      <img alt={song.name} src={song.cover} />
+      <div className="song-description">
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
